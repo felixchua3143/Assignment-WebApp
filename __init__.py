32575ac6b3e0d17ecfbdf09e5a565d17bccfb149
felixchua3141/2/3 <132286580+felixchua3143@users.noreply.github.com>
2025-01-20 09:36:48 +0800

    s

--- __init__.py
+++ __init__.py
@@ -6,8 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('/templates/index.html')
 
+@app.route('/')
+def carbon_cal():
+    return render_template('/templates/carbon_calculator.html')
 
 
 if __name__ == '__main__':
