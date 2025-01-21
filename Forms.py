1b50e6a3b84edd3a4fca5f117f1a5b2c4e41e0df
felixchua3141/2/3 <132286580+felixchua3143@users.noreply.github.com>
2025-01-21 10:59:12 +0800

    abc

--- Forms.py
+++ Forms.py
@@ -1,10 +1,10 @@
 from wtforms import Form, StringField, IntegerField, SelectField, SelectMultipleField, validators
 
-class CreateUserForm(Form):
-    first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
 
 class CarbonCalForm(Form):
-    electricity = SelectField("Electricity Usage ($)", [validators.InputRequired])
-    gas = SelectField("Gas ($)", validators.InputRequired)
-    water = SelectField("Water ($)", validators.InputRequired)
-    num_household = IntegerField("Number of People In Household", validators.InputRequired)
+    electricity = SelectField("Electricity Usage", choices=[("e < $50", "< $50"), ("e $50 - $100","$50 - $100"), ("e $101 - $150", "$101 - $150"), ("e $151 - $200", "$151 - $200"), ("e > $200", "> $200")])
+    gas = SelectField("Gas", choices=[("g $0", "$0"), ("g < $10", "< $10"), ("g $11 - $15", "$11 - $15"), ("g $16 - $20", "$16 - $20"), ("g > $20", "> $20")])
+    water = SelectField("Water", choices=[("w < $25", "< $25"), ("w $25 - $50", "$25 - $50"), ("w $51 - $70", "$51 - $70"), ("w $71 - $90", "$71 - $90"), ("w > $90", "> $90")])
+    num_household = IntegerField("Number of People In Household")
+    food = SelectMultipleField("Food (ctrl+click to select multiple options", choices=[("beef", "Beef"), ("eggs", "Eggs"), ("pork", "Pork"), ("vegetables", "Vegetables"), ("chicken_duck", "Chicken and Duck"), ("fruits", "Fruits"), ("mutton", "Mutton"), ("grains","Grains"), ("seafood", "Seafood"), ("dairy_prod", "Dairy Products")])
+
--- __init__.py
+++ __init__.py
@@ -6,36 +6,16 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-@app.route('/carbon_cal.html')
-def carbon_cal():
-    carbon_cal_form = CarbonCalForm(request.form)
-    if request.method == 'POST' and carbon_cal_form.validate():
-        carbon_dict = {}
-        db = shelve.open('carbon_cal.db', 'c')
+@app.route('/carbon_cal.html', methods=["GET", "POST"])
+def create_cal():
+    create_cal_form = CarbonCalForm(request.form)
+    if request.method == "POST" and create_cal_form.validate():
+        return redirect(url_for("create_cal"))
+    return render_template("carbon_cal.html", form=create_cal_form)
 
-        try:
-            carbon_dict = db['carbon_cal']
-        except:
-            print("Error in retrieving data from carbon_cal.db")
-
-        carbon_cal = CarbonCalForm(carbon_cal_form.get_user_id,carbon_cal_form.electricity, carbon_cal_form.gas, carbon_cal_form.water, carbon_cal_form.num_household)
-        carbon_dict[CarbonCalForm.get_user_id()] = carbon_cal
-        db['carbon_cal'] = carbon_dict
-
-        # Test codes
-        carbon_dict = db['carbon_cal']
-        CarbonCalForm = carbon_dict[CarbonCalForm.get_user_id()]
-        print(user.get_first_name(), user.get_last_name(), "was stored in user.db successfully with user_id ==",
-              user.get_user_id())
-
-        db.close()
-
-        return redirect(url_for('home'))
-    return render_template('carbon_cal.html', form=carbon_cal_form)
-    return render_template('carbon_cal.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
